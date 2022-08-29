b37b1e1b6f15c6797bd5e32265b32a34b751043b
MiaLaFleur <mia.lafleur@yahoo.com>
2022-08-28 22:27:33 -0500

    add About component

---
+++ src/Components/About/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <section>
+      <div id="about">
+        <h1>About Me</h1>
+      </div>
+      <div>
+        <img src={require('../../Assets/imgs/picOfMe.jpg')} className="picOfMe" alt="profile" />
+      </div>
+      <div>
+        <p>My name is Mia LaFleur! I am from North Texas and I am ready to begin my career in software development. I am very good at working with a team and quickly picking up new concepts and technologies. I am very creative and excited to learn and constantly be improving my new coding abilities!</p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
