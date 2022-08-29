4366b566caa6255970d880aa6b6c4a9f79b334d9
MiaLaFleur <mia.lafleur@yahoo.com>
2022-08-28 22:48:01 -0500

    add Header and child Nav component.. also created Hook in App.js

---
+++ src/Components/Header/Nav.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Nav(props) {
+
+  const { currentTab, setCurrentTab } = props;
+
+  return (
+    <nav>
+      <ul>
+        <li className={currentTab === "about" ? "mx-2 navActive" : "mx-2"}>
+          <span onClick={() => setCurrentTab("about")}>About Me</span>
+        </li>
+        <li className={currentTab === "portfolio" ? "mx-2 navActive" : "mx-2"}>
+          <span onClick={() => setCurrentTab("portfolio")}>My Portfolio</span>
+        </li>
+        <li className={currentTab === "resume" ? "mx-2 navActive" : "mx-2"}>
+          <span onClick={() => setCurrentTab("resume")}>My Resume</span>
+        </li>
+        <li className={currentTab === "contact" ? "mx-2 navActive" : "mx-2"}>
+          <span onClick={() => setCurrentTab("contact")}>Contact Me</span>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
